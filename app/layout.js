c613601eb711a19b9c338ea32283e1911088ba86
Sheetinvoicer <info@sheetinvoicer.com>
2025-11-08 19:47:45 +0300

    Add RootLayout component with metadata

---
+++ app/layout.js
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'SheetInvoicer - Turn spreadsheets into invoices',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}

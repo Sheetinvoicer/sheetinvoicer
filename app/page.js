72b0ee73481551194ee818cb1c7d0c660cfef628
Sheetinvoicer <info@sheetinvoicer.com>
2025-11-08 19:06:32 +0300

    Create Home component for initial page layout

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 50, textAlign: 'center' }}>
+      <h1>SheetInvoicer TEST 🚀</h1>
+      <p>If you see this, deployment works!</p>
+    </div>
+  )
+}
